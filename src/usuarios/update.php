20ba54b7e690c4cc18111e9b39e067f11d0a6230
João Vitor da Mota Nogueira <jvnm3365@gmail.com>
2025-10-30 20:05:47 -0300

    Update update.php

--- src/usuarios/update.php
+++ src/usuarios/update.php
@@ -4,8 +4,8 @@ require '../../db.php';
 $id = basename($_SERVER['REQUEST_URI']);
 $data = json_decode(file_get_contents("php://input"), true);
 
-$sql = "UPDATE tasks SET title = ?, description = ?, status = ? WHERE id = ?";
+$sql = "UPDATE user_manager SET name = ?, email = ?, phone = ?, address = ? WHERE id = ?";
 $stmt = $pdo->prepare($sql);
-$stmt->execute([$data['title'], $data['description'], $data['status'], $id]);
+$stmt->execute([$data['name'], $data['email'], $data['phone'], $data['address'], $id]);
 
-echo json_encode(['message' => 'Tarefa atualizada']);
+echo json_encode(['message' => 'Usuário atualizado']);
