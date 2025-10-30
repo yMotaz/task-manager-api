e37b5321ae0b1c91313a47eadad04cf46dadeddb
João Vitor da Mota Nogueira <jvnm3365@gmail.com>
2025-10-30 19:59:12 -0300

    Delete src directory

--- src/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'db.php';
-$id = basename($_SERVER['REQUEST_URI']);
-$stmt = $pdo->prepare("DELETE FROM usuarios WHERE id = ?");
-$stmt->execute([$id]);
-echo json_encode(['mensagem' => 'Usuário deletado com sucesso']);
-?>
--- src/read.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'db.php';
-$stmt = $pdo->query("SELECT * FROM usuarios");
-$usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($usuarios);
-?>
