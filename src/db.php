f691ab48a1297abf15b8f83978ec89cc244cbe03
João Vitor da Mota Nogueira <jvnm3365@gmail.com>
2025-10-30 19:24:54 -0300

    Add database connection script in db.php

---
+++ src/db.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$db = 'user_manager';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erro na conexão: " . $e->getMessage();
+    exit;
+}
+?>
