e5f1d30b2453d61f01fb8063f66dac67cebb46a3
João Vitor da Mota Nogueira <jvnm3365@gmail.com>
2025-10-30 19:25:29 -0300

    Implement routing for user operations in index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$request = $_SERVER['REQUEST_URI'];
+switch ($request) {
+    case '/usuarios' && $_SERVER['REQUEST_METHOD'] === 'GET':
+        require __DIR__ . '/src/read.php';
+        break;
+    case '/usuarios' && $_SERVER['REQUEST_METHOD'] === 'POST':
+        require __DIR__ . '/src/create.php';
+        break;
+    case (preg_match('/\/usuarios\/\d+/', $request) ? true : false) && $_SERVER['REQUEST_METHOD'] === 'PUT':
+        require __DIR__ . '/src/update.php';
+        break;
+    case (preg_match('/\/usuarios\/\d+/', $request) ? true : false) && $_SERVER['REQUEST_METHOD'] === 'DELETE':
+        require __DIR__ . '/src/delete.php';
+        break;
+    default:
+        http_response_code(404);
+        echo json_encode(['erro' => 'Rota não encontrada']);
+        break;
+}
+?>
