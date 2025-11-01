0a6cdc2db343bcbb19d83aa9e93231f2213d0c60
yMotaz <jvnm3365@gmail.com>
2025-11-01 10:59:11 -0300

    Atualizando todos do Status

--- teste/run-tests.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "Testando criação de usuário...\n";
-// Simular requisição POST, verificar resposta, etc.
-// Repetir para leitura, atualização e exclusão
-?>
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'aec34bc26e5b9e32021b2dc2180c16d095da8f8d',
+        'reference' => '92a457f1025ddf4bd79b19ea0beffad954a88249',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'aec34bc26e5b9e32021b2dc2180c16d095da8f8d',
+            'reference' => '92a457f1025ddf4bd79b19ea0beffad954a88249',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
